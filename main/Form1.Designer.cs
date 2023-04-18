b79d4a76c1715eca5fcefcf35274f01860d741fa
AnnaPustelnyk <anna46xk8@gmail.com>
2023-04-18 10:06:41 +0200

    only green

--- main/Form1.Designer.cs
+++ main/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             pictureBox1 = new PictureBox();
             button1 = new Button();
+            button2 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -53,11 +54,22 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // button2
+            // 
+            button2.Location = new Point(321, 27);
+            button2.Name = "button2";
+            button2.Size = new Size(215, 27);
+            button2.TabIndex = 2;
+            button2.Text = "only green";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(700, 338);
+            Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(pictureBox1);
             Margin = new Padding(3, 2, 3, 2);
@@ -71,5 +83,6 @@
 
         private PictureBox pictureBox1;
         private Button button1;
+        private Button button2;
     }
 }
--- main/Form1.cs
+++ main/Form1.cs
@@ -18,5 +18,36 @@ namespace main
                 pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             }
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+
+            //get image dimension
+            int width = bmp.Width;
+            int height = bmp.Height;
+
+            Bitmap gbmp = new Bitmap(bmp);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color p = bmp.GetPixel(x, y);
+
+                    if (p.G > p.R && p.G > p.B)
+                    {
+                        gbmp.SetPixel(x, y, p);
+                    }
+                    else
+                    {
+                        gbmp.SetPixel(x, y, Color.Black);
+                    }
+
+                }
+            }
+
+            pictureBox1.Image = gbmp;
+        }
     }
 }
